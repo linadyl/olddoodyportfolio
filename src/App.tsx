5b8471b0e00a04c047a748b3d9052dd955b31154
lina-sheridanimm <leedoy@sheridancollege.ca>
2025-03-23 21:44:30 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css'
+import logo from './assets/logo.svg'
+
+function App() {
+
+    const [isRotating, setIsRotating] = useState(false);
+
+  return (
+    <div className='App'>
+
+        <header className='App-header'>
+            <div className='logo-container'
+                onMouseEnter={() => setIsRotating(true)}
+                onMouseLeave={() => setIsRotating(false)}>
+                    <img src={logo} className={`logo ${isRotating ? 'rotating' : ''}` }
+                        alt="logo image"/>
+            </div>
+        </header>
+
+        <div className='body-container'>
+
+            <div className='tagline-container'>
+                <p>hi! my name is <span style={{ color:'var(--accent-colour)'}}>lina</span> and i'm an (aspiring) <span style={{ color:'var(--accent-colour)'}}>ux/product engineer 🥸</span></p>
+            </div>
+        </div>
+
+    </div>
+)
+}
+
+export default App
