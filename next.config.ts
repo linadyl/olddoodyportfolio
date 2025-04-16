30b14abea5717d4072b392bfcf29bf30b60e4875
lina-sheridanimm <leedoy@sheridancollege.ca>
2025-04-16 17:36:01 -0400

    deploy bugs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  experimental: {
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
